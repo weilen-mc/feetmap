39500de8b874cacf6bc7490f1994fa13247d3ae2
Christoph Weilenmann <chriswei@protonmail.com>
2026-03-08 00:20:14 +0100

    changed build script to autogenerate superuser. Removed image files from git repo.

--- render-build.sh
+++ render-build.sh
@@ -6,3 +6,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create superuser if environment variables are set
+if [[ $DJANGO_SUPERUSER_USERNAME ]]; then
+  python manage.py createsuperuser --no-input \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL" || true
+fi
