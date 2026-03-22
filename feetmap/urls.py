473120cc00507a8f3d2072c21c66eb2cd50b6d7f
Christoph Weilenmann <chriswei@protonmail.com>
2026-03-22 16:48:45 +0100

    change to url file for media handling

--- feetmap/urls.py
+++ feetmap/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,11 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Serve media files locally via Django even when DEBUG=False 
+    # (only if Cloudinary isn't active, perfect for Pi deployment)
+    if not os.environ.get('CLOUDINARY_URL'):
+        urlpatterns += [
+            re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        ]
+
